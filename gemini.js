1f822657df79a472f964fe3bc6c5e3fc1f9c0d8d
acquahsamuel <iamsamuelacquah@gmail.com>
2024-05-31 11:06:36 +0000

    WIP] +>  code refactoring

---
+++ gemini.js
@@ -0,0 +1,19 @@
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEYS);
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+  const prompt = "Write generic best SEO post description  for LG 55 Inches 4K TV";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+
+run();
